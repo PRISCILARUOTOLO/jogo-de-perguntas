7a01770a3315b6b65e742da075d81b00015b451c
PRISCILA RUOTOLO <126161177+PRISCILARUOTOLO@users.noreply.github.com>
2025-11-03 09:47:13 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,1 +1,9 @@
 
+print("Bom diaaaaaaaaaaaaaa....")
+
+#Descrição 
+print(" Jogo das Palavras ")
+#Perguntas
+print("Qual é seu nome? ")
+nome = input(print("Digite seu nome: "))
+#jogo das perguntas 
