f6d232c49f1093af53e7cfc1e1ad49154b9f4316
PRISCILA RUOTOLO <126161177+PRISCILARUOTOLO@users.noreply.github.com>
2025-11-17 08:34:18 -0300

    Create jogo.py

---
+++ jogo.py
@@ -0,0 +1,18 @@
+#Colocar título e descrever o jogo.
+print("JOGO DE PERGUNTAS")
+
+print("O jogo será trabalhado na forma de utilizar dados externos, para desenvolver respostas e novas perguntas, utilizando a idéia de INPUT")
+#Pensar em um tema...
+nome = input("Qual é seu nome?")
+print("Oi" ,nome)
+
+pontos = 0;
+
+#pergunta1
+matemático = input("Qual foi o matemático que desenvolveu a seguinte fórmula, a² = b² + c²")
+if matemático == "Pitágoras" or matemático == "pitagoras":
+     print("Parabéns", nome,",você acertou.🤓")
+     pontos += 1
+else: 
+     print("Que pena", nome, "você errou 🤔")
+     
